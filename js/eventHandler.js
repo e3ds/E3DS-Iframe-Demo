5be3cac1bf1c1ca212625de8247542cc0f308ec7
robinmollah <robinsajin@gmail.com>
2021-03-09 03:41:30 +0600

    Example of an iframe

--- js/eventHandler.js
+++ js/eventHandler.js
@@ -1,3 +1,14 @@
 window.e3ds.onEvent("increaseSessionExpireTime", (data) => {
 	console.log("Increasing session expire time", data);
+
+	// Example of iframe
+	let iframe = document.createElement("iframe");
+	iframe.src="http://example.com";
+	iframe.classList.add("iframeStyle");
+	document.getElementsByTagName("body")[0].append(iframe);
+
+	// Example of a button
+	let button1 = document.createElement("button");
+	button1.innerText = "Click here";
+	document.getElementsByTagName("body")[0].append(button1);
 });
