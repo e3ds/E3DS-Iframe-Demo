956d7937efa67e14330c04cfc6384abafd15f327
robinmollah <robinsajin@gmail.com>
2021-03-09 03:57:29 +0600

    Close button added

--- js/eventHandler.js
+++ js/eventHandler.js
@@ -5,10 +5,15 @@ window.e3ds.onEvent("increaseSessionExpireTime", (data) => {
 	let iframe = document.createElement("iframe");
 	iframe.src="http://example.com";
 	iframe.classList.add("iframeStyle");
+	iframe.style.display = "block";
 	document.getElementsByTagName("body")[0].append(iframe);
 
 	// Example of a button
 	let button1 = document.createElement("button");
-	button1.innerText = "Click here";
+	button1.innerText = "Close Iframe";
+	button1.classList.add("closeButtonOfIframe");
+	button1.addEventListener("click", (ev) => {
+		iframe.style.display = "none";
+	});
 	document.getElementsByTagName("body")[0].append(button1);
 });
