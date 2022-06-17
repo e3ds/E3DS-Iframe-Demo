dbb202f53cad8468acb9002feaaf7b1dc31c96e3
Mandeep Singhmar <msingh8328@gmail.com>
2022-06-17 15:46:26 +0530

    feat: react demo

---
+++ react-eagle-demo/src/App.js
@@ -0,0 +1,79 @@
+import { useRef } from 'react';
+import './App.css';
+import { commands } from './utils';
+import Logo from './Unreal_Logo.png';
+
+function App() {
+  const iframeElem = useRef();
+
+  const sendToMainPage = (obj) => {
+    let origin = '*';
+    console.log(iframeElem);
+    if (null !== iframeElem.current) {
+      iframeElem.current.contentWindow.postMessage(JSON.stringify(obj), origin);
+    }
+  };
+  function switchTo(val) {
+    let descriptor = {
+      Teleport: val,
+    };
+    let obj = {
+      cmd: 'sendToUe4',
+      value: descriptor,
+    };
+    sendToMainPage(obj);
+  }
+
+  return (
+    <div style={{ display: 'flex', overflow: 'hidden' }}>
+      <div>
+        <div style={{ background: 'black', textAlign: 'center' }}>
+          <img
+            style={{ width: '50px', height: '50px' }}
+            src={Logo}
+            alt='logo'
+          />
+        </div>
+
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '20px',
+            padding: '10px',
+          }}
+        >
+          {commands.map((item) => (
+            <button
+              style={{
+                background: 'white',
+                outline: 'none',
+                border: '1px solid gray',
+                fontSize: '17px',
+                cursor: 'pointer',
+                padding: '6px',
+              }}
+              onClick={() => switchTo(item.value)}
+            >
+              {item.name}
+            </button>
+          ))}
+        </div>
+      </div>
+
+      <div style={{ height: '100vh', width: '100%' }}>
+        <iframe
+          ref={iframeElem}
+          title='demo'
+          id='iframe_1'
+          src='https://connector.eagle3dstreaming.com/v5/demo/FeaturesTemplate/E3DS-Iframe-Demo'
+          width='100%'
+          height='100%'
+          allowfullscreen
+        ></iframe>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-eagle-demo/src/utils/index.js
@@ -0,0 +1,55 @@
+export const commands = [
+  {
+    name: 'Open URL',
+    value: '1',
+  },
+  {
+    name: 'Open Mail App',
+    value: '2',
+  },
+  {
+    name: 'Open Mail App',
+    value: '2',
+  },
+  {
+    name: 'Open Call',
+    value: '3',
+  },
+  {
+    name: 'Open Keyboard',
+    value: '4',
+  },
+  {
+    name: 'Toggle Browser Mouse',
+    value: '5',
+  },
+  {
+    name: 'Increase Session Timer',
+    value: '6',
+  },
+
+  {
+    name: 'Get User Info',
+    value: '7',
+  },
+  {
+    name: 'Get User Device Info',
+    value: '8',
+  },
+  {
+    name: 'Video Player',
+    value: '9',
+  },
+  {
+    name: 'Audio Player',
+    value: '10',
+  },
+  {
+    name: 'Command Line Argument Pass',
+    value: '11',
+  },
+  {
+    name: 'Redirect URL',
+    value: '12',
+  },
+];
